f35711e279688c784e2207d34cd964cf5f9f9d25
VollRagm <vollragm@gmail.com>
2023-05-10 15:26:48 +0200

    Created initial backend

---
+++ api/Database.php
@@ -0,0 +1,80 @@
+<?php
+
+include "config.php";
+
+class Database
+{
+    protected $connection = null;
+ 
+    public function __construct()
+    {
+        try {
+            $this->connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
+         
+            if ( mysqli_connect_errno()) {
+                throw new Exception("Could not connect to database.");   
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());   
+        }           
+    }
+ 
+    public function select($query = "" , $type, $params = [])
+    {
+        try {
+            $stmt = $this->executeStatement( $query, $type, $params );
+            $result = $stmt->get_result();          
+            $stmt->close();
+ 
+            return $result;
+        } catch(Exception $e) {
+            throw New Exception( $e->getMessage() );
+        }
+        return false;
+    }
+
+    public function exec($query = "", $type = "", $params = []){
+        try {
+            $stmt = $this->connection->prepare( $query );
+
+            if($stmt === false) {
+                throw New Exception("Unable to do prepared statement: " . $query);
+            }
+ 
+            if( $params ) {
+                $stmt->bind_param($type, ...$params);
+            }
+ 
+            $result = $stmt->execute();
+           
+            $stmt->close();
+            return $result;
+
+        } catch(Exception $e) {
+            throw New Exception( $e->getMessage() );
+        }   
+    }
+ 
+    private function executeStatement($query = "", $type = "", $params = [])
+    {
+        try {
+            $stmt = $this->connection->prepare($query);
+
+        if ($stmt === false) {
+            throw new Exception("Unable to do prepared statement: " . $query);
+        }
+
+        if ($params) {
+            $stmt->bind_param($type, ...$params);
+        }
+
+        $stmt->execute();
+    
+        return $stmt;
+
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+}
+?>
---
+++ api/config.php
@@ -0,0 +1,6 @@
+<?php
+define("DB_HOST", "62.171.166.53");
+define("DB_USERNAME", "dhbw2");
+define("DB_PASSWORD", "campusconnect69420");
+define("DB_DATABASE_NAME", "dhbwsv");
+?>
