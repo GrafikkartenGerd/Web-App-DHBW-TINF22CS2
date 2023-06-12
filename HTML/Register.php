45552648376128a10d67b1c51b427f9fbfcb356b
MrSebas3 <120368945+MrSebas3@users.noreply.github.com>
2023-06-12 03:03:16 +0200

    fix scrollbar

--- HTML/Register.php
+++ HTML/Register.php
@@ -199,6 +199,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </style>
 </head>
 <body>
+
     <header class="header">
         <img src="logo.png" alt="Logo">
         <span class="logo-text">CampusConnect</span>
