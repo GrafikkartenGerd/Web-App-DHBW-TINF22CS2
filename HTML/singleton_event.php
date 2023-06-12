47ba66944eee4e0ee2ae13552eb77c71b67da414
MrSebas3 <120368945+MrSebas3@users.noreply.github.com>
2023-06-12 03:31:24 +0200

    Update singleton_event.php

--- HTML/singleton_event.php
+++ HTML/singleton_event.php
@@ -32,7 +32,7 @@ $attendees = [
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="header.css">
-
+  <link rel="stylesheet" href="footer.css">
   <style>
     .event-container {
         max-width: 800px;
@@ -93,6 +93,7 @@ $attendees = [
 </head>
 <body>
   <?php include("header.php"); ?>
+  <main>
   <div class="container mt-4">
     <div class="row">
       <div class="col">
@@ -127,7 +128,11 @@ $attendees = [
       </div>
     </div>
   </div>
+</main>
 
+<?php
+  include("footer.php")
+?>
   <!-- Include Bootstrap JS and any other necessary JS files -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
