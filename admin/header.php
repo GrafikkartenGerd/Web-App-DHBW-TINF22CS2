9e9e0b50a2b3635e41d8e9cf50551efc20c7910e
Egzon284 <egzon2804@gmail.com>
2023-06-13 20:55:38 +0200

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="../HTML/index.php">
     <img src="logo.png" alt="Campus Connect Icon" class="navbar-icon">
+    <link rel="icon" type="image/x-icon" href="/html/favicon.ico">
+
     <span class="header">CampusConnect</span>
   </a>
   <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
